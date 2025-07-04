6b7de1dfe0444fa0b6b0a4cfd6ea09bf548efa12
Peter_Richards <rickpetes84@gmail.com>
2025-07-04 14:48:44 +0300

    second commit

--- properties/properties/urls.py
+++ properties/properties/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from property.views import PropertyView  # <-- Correct import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/properties/', views.PropertyView, name='property-list'),
-    path('api/property/<int:pk>/', views.PropertyView, name='Property-detail'),
+    path('api/properties/', PropertyView.as_view(), name='property-list'),
+    path('api/property/<int:pk>/', PropertyView.as_view(), name='Property-detail'),
 ]
--- properties/property/views.py
+++ properties/property/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Property
+from .serializers import PropertySerializer
+
+
+class PropertyView(APIView):
+    def get(self, request, pk=None):
+        if pk is not None:
+            try:
+                property_obj = Property.objects.get(pk=pk)
+            except Property.DoesNotExist:
+                return Response({'error': 'Property not found.'}, status=status.HTTP_404_NOT_FOUND)
+            serializer = PropertySerializer(property_obj)
+            return Response(serializer.data)
+        else:
+            properties = Property.objects.all()
+            serializer = PropertySerializer(properties, many=True)
+            return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PropertySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        if pk is not None:
+            try:
+                property_obj = Property.objects.get(pk=pk)
+            except Property.DoesNotExist:
+                return Response({'error': 'Property not found.'}, status=status.HTTP_404_NOT_FOUND)
+            serializer = PropertySerializer(property_obj, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Please provide a valid Property ID.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):
+        if pk is not None:
+            try:
+                property_obj = Property.objects.get(pk=pk)
+            except Property.DoesNotExist:
+                return Response({'error': 'Property not found.'}, status=status.HTTP_404_NOT_FOUND)
+            property_obj.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            Property.objects.all().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
